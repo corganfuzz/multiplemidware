c07deb9472ebbb566ae7909dd605580e62a13785
Gerald Ruiz <gruiz@eogresources.com>
2019-10-07 16:51:12 -0500

    2 checks in place

--- main.go
+++ main.go
@@ -3,12 +3,49 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type city struct {
 	Name string
 	Area uint64
+}
+
+//MiddleWare to check if content type as JSON
+
+func filterContentType(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Currently midware is checking content type ")
+
+		// Filtering requests by MIME type
+
+		if r.Header.Get("Content-Type") != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte("415 - Unsupported Media Type. Only JSON accepted"))
+			return
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
+// Middlerware to add server timestamp for response cookie
+
+func setServerTimeCookie(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+
+		//Setting cookie to each and every response
+
+		cookie := http.Cookie{
+			Name:  "Server-Time(UTC)",
+			Value: strconv.FormatInt(time.Now().Unix(), 10),
+		}
+		http.SetCookie(w, &cookie)
+		log.Println("Currently in the set server time middleware")
+	})
 }
 
 func mainLogic(w http.ResponseWriter, r *http.Request) {
@@ -18,21 +55,20 @@ func mainLogic(w http.ResponseWriter, r *http.Request) {
 		var tempCity city
 		decoder := json.NewDecoder(r.Body)
 		err := decoder.Decode(&tempCity)
+
 		if err != nil {
 			panic(err)
 		}
 		defer r.Body.Close()
 
 		//Resource Creatin logic goes here
-
 		fmt.Printf("Got %s city with area of %d sq miles!\n", tempCity.Name, tempCity.Area)
-
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("201 - Created"))
+
 	} else {
 
 		//Method not Allowed
-
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("405 - Method not Allowed"))
 	}
@@ -40,6 +76,7 @@ func mainLogic(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	fmt.Println("Server is running in port 8000...")
-	http.HandleFunc("/city", mainLogic)
+	mainLogicHandler := http.HandlerFunc(mainLogic)
+	http.Handle("/city", filterContentType(setServerTimeCookie(mainLogicHandler)))
 	http.ListenAndServe(":8000", nil)
 }
